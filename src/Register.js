730fbc874008e4b28ec900d34704de591f8ab3b1
Smit Butle <smit2003.sb@gmail.com>
2023-07-24 18:31:42 +0530

    .

--- src/Register.js
+++ src/Register.js
@@ -49,7 +49,8 @@ const Register = () => {
             setqrVisibility({ display: "block" })
             setplaceholderVisibility({ display: "none" })
             // setqrLink("upi://pay?pa=utkarsha.1977.usb@okhdfcbank&pn=Utkarsha%20Butle&am=1.00&cu=INR&tn=LD%2D" + encodeURI(value))
-            setqrLink("upi://pay?pa=smit2003.sb@okicici&pn=Smit%20Butle&am=1.00&cu=INR&aid=uGICAgIDAhMyvVQ&tn=LD%2D" + encodeURI(value))
+            // setqrLink("upi://pay?pa=smit2003.sb@okicici&pn=Smit%20Butle&am=1.00&cu=INR&aid=uGICAgIDAhMyvVQ&tn=LD%2D" + encodeURI(value))
+            setqrLink("upi://pay?pa=smit2003.sb@okicici&pn=Smit%20Butle&am=1&tn=LD%2D" + encodeURI(value))
 
         }
         else {
