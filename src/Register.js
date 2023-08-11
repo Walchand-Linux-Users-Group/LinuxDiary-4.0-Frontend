c3f0dcd6899a7cd7d133cd5c13fa80547f1c74d5
Smit Butle <smit2003.sb@gmail.com>
2023-08-11 10:57:01 +0530

    render up

--- src/Register.js
+++ src/Register.js
@@ -196,193 +196,193 @@ const Register = () => {
         //   console.log(error);
     }
 
-    return (
-        <div className="flex-container" id='register'>
-            <h2>If you have paid the amount and didn't recieve confirmation email,<br /> please contact Datta Gangji +91 75177 56075</h2>
-            <h1 className='pink'>Registrations are temporary down.</h1>
-        </div>
-
-    )
     // return (
     //     <div className="flex-container" id='register'>
-    //         <form className='form' onSubmit={handleSubmit}>
-    //             <label htmlFor="name" className="input-labels">
-    //                 {" "}
-    //                 Name{" "}
-    //             </label>
-    //             <input
-    //                 required=""
-    //                 placeholder="Your Name"
-    //                 name="name"
-    //                 id="name"
-    //                 type="text"
-    //                 // onChange={handleChangeName}
-    //                 onChange={handleChange}
-    //             />
-    //             <label htmlFor="email" className="input-labels">
-    //                 {" "}
-    //                 Email ID{" "}
-    //             </label>
-    //             <input
-    //                 required=""
-    //                 placeholder="Your Email"
-    //                 name="email"
-    //                 id="email"
-    //                 type="email"
-
-    //                 onChange={handleChange}
-    //             />
-    //             <label htmlFor="phone" className="input-labels">
-    //                 {" "}
-    //                 Phone Number{" "}
-    //             </label>
-    //             <input
-    //                 required=""
-    //                 placeholder="Your Phone Number"
-    //                 name="phone"
-    //                 id="phone"
-    //                 type="tel"
-
-    //                 onChange={handleChange}
-    //             />
-    //             <label htmlFor="collegeName" className="input-labels">
-    //                 {" "}
-    //                 College Name{" "}
-    //             </label>
-    //             <input
-    //                 required=""
-    //                 placeholder="Your College Name"
-    //                 name="collegeName"
-    //                 id="collegeName"
-    //                 type="text"
-    //                 onChange={handleChange}
-    //             />
-    //             {/* <label htmlFor="branch" className="input-labels" id='dont-display'>
-    //                 {" "}
-    //                 Branch{" "}
-    //             </label>
-    //             <br /> */}
-
-    //             {/* <div className='selectdropdown' id='dont-display'>
-    //                 <select id="branch" name="branch" required=""
-    //                     onChange={handleChange} class="mySelectArrow">
-    //                     <option value="" disabled selected id='dont-display'>
-    //                         Select your option
-    //                     </option>
-    //                     <option value="CS">Computer Science Engineering</option>
-    //                     <option value="IT">Information Technology Engineering</option>
-    //                     <option value="ELECTRONICS">Electronics Engineering</option>
-    //                     <option value="ELECTRICAL">Electrical Engineering</option>
-    //                     <option value="MECHANICAL">Mechanical Engineering</option>
-    //                     <option value="CIVIL">Civil Engineering</option>
-    //                     <option value="OTHERS">Others</option>
-    //                 </select>
-    //             </div> */}
-    //             <label htmlFor="yearOfStudy" className="input-labels">
-    //                 {" "}
-    //                 Year of Study{" "}
-    //             </label>
-    //             <br />
-
-    //             <div className='selectdropdown'>
-    //                 <select id="yearOfStudy" name="yearOfStudy" required=""
-    //                     onChange={handleChange} class="mySelectArrow">
-    //                     <option value="" disabled selected id='dont-display'>
-    //                         Select your option
-    //                     </option>
-    //                     <option value="First Year">First Year</option>
-    //                     <option value="Second Year">Second Year</option>
-    //                     <option value="Third Year">Third Year</option>
-    //                     <option value="Fourth Year">Fourth Year</option>
-    //                     <option value="Other">Other</option>
-    //                 </select>
-    //             </div>
-    //             <label htmlFor="isDualBooted" className="input-labels">
-    //                 Do you have dual booted laptop ?{" "}
-    //             </label>
-    //             <br />
-    //             <div className='selectdropdown'>
-    //                 <select id="isDualBooted" name="isDualBooted" required="" onChange={handleChange} class="mySelectArrow">
-    //                     <option value="" disabled selected id='dont-display'>
-    //                         Select your option
-    //                     </option>
-    //                     <option value="Yes">Yes</option>
-    //                     <option value="No">No</option>
-    //                 </select>
-    //             </div>
-    //             <label htmlFor="transactionId" className="input-labels">
-    //                 {" "}
-    //                 Transaction ID{" "}
-    //             </label>
-    //             <input
-    //                 required=""
-    //                 placeholder="Payment Transaction ID"
-    //                 name="transactionId"
-    //                 id="transactionId"
-    //                 type="text"
-    //                 onChange={handleChange}
-    //             />
-    //             <label htmlFor="referalCode" className="input-labels">
-    //                 {" "}
-    //                 Referral Code{" "}
-    //             </label>
-    //             <input
-    //                 required=""
-    //                 placeholder="Referral Code"
-    //                 name="referalCode"
-    //                 id="referalCode"
-    //                 type="text"
-    //                 onChange={handleChange}
-    //             />
-    //             <div className='submitSection'>
-    //                 {!isLoading && (<div className='submitButton'>
-    //                     <input type="submit" defaultValue="REGISTER" className='btn-hover color-5' />
-    //                 </div>
-    //                 )}
-    //                 {isLoading && (
-    //                     <ProgressBar
-    //                         height="160"
-    //                         width="auto"
-    //                         ariaLabel="progress-bar-loading"
-    //                         wrapperStyle={{}}
-    //                         wrapperClass="progress-bar-wrapper"
-    //                         borderColor='#00d12d'
-    //                         barColor='#ffffff'
-    //                     />
-    //                 )}
-
-    //             </div>
-    //         </form>
-    //         <div className='qr-div'>
-    //             {/* <img src={qr_img_src} className="qr" />
-    //             <a href={qrLink}>
-    //             <QRCode value={qrLink} className='qrWithName'></QRCode>
-    //         </a> */}
-    //             <div className='qr-section'>
-    //                 <div className='qr-text-section'>
-    //                     {/* <p className='qr-text-heading'>Scan <span>/ Tap </span>QR to pay</p> */}
-    //                     <p className='qr-text-heading'>Scan QR to pay</p>
-    //                     <p className='qr-text-info'>
-    //                         UPI ID: dattnareshgangji21@okaxis<br /><br />
-    //                         <span>Amount: ₹‎199.00</span></p>
-    //                 </div>
-    //                 {/* <div style={placeholderVisibility}>
-    //                     <p className='qr-text-placeholder'>Enter Name <br /> to view QR</p>
-    //                 </div> */}
-    //                 {/* <div style={qrVisibility}> */}
-    //                 <div>
-    //                     {/* <a href={qrLink}> */}
-    //                     {/* <a href="upi://pay?pa=dattnareshgangji21@okaxis&pn=Datta%20Gangji&am=1.00&cu=INR&aid=uGICAgID3ib3mVA"> */}
-    //                     <img src="./images/qr.png" className="qr2" alt='qr' />
-    //                     {/* <QRCode value={qrLink} className='qr'>
-    //                         </QRCode>*/}
-    //                     {/* </a>  */}
-    //                 </div>
-    //             </div>
-    //         </div>
+    //         <h2>If you have paid the amount and didn't recieve confirmation email,<br /> please contact Datta Gangji +91 75177 56075</h2>
+    //         <h1 className='pink'>Registrations are temporary down.</h1>
     //     </div>
 
     // )
+    return (
+        <div className="flex-container" id='register'>
+            <form className='form' onSubmit={handleSubmit}>
+                <label htmlFor="name" className="input-labels">
+                    {" "}
+                    Name{" "}
+                </label>
+                <input
+                    required=""
+                    placeholder="Your Name"
+                    name="name"
+                    id="name"
+                    type="text"
+                    // onChange={handleChangeName}
+                    onChange={handleChange}
+                />
+                <label htmlFor="email" className="input-labels">
+                    {" "}
+                    Email ID{" "}
+                </label>
+                <input
+                    required=""
+                    placeholder="Your Email"
+                    name="email"
+                    id="email"
+                    type="email"
+
+                    onChange={handleChange}
+                />
+                <label htmlFor="phone" className="input-labels">
+                    {" "}
+                    Phone Number{" "}
+                </label>
+                <input
+                    required=""
+                    placeholder="Your Phone Number"
+                    name="phone"
+                    id="phone"
+                    type="tel"
+
+                    onChange={handleChange}
+                />
+                <label htmlFor="collegeName" className="input-labels">
+                    {" "}
+                    College Name{" "}
+                </label>
+                <input
+                    required=""
+                    placeholder="Your College Name"
+                    name="collegeName"
+                    id="collegeName"
+                    type="text"
+                    onChange={handleChange}
+                />
+                {/* <label htmlFor="branch" className="input-labels" id='dont-display'>
+                    {" "}
+                    Branch{" "}
+                </label>
+                <br /> */}
+
+                {/* <div className='selectdropdown' id='dont-display'>
+                    <select id="branch" name="branch" required=""
+                        onChange={handleChange} class="mySelectArrow">
+                        <option value="" disabled selected id='dont-display'>
+                            Select your option
+                        </option>
+                        <option value="CS">Computer Science Engineering</option>
+                        <option value="IT">Information Technology Engineering</option>
+                        <option value="ELECTRONICS">Electronics Engineering</option>
+                        <option value="ELECTRICAL">Electrical Engineering</option>
+                        <option value="MECHANICAL">Mechanical Engineering</option>
+                        <option value="CIVIL">Civil Engineering</option>
+                        <option value="OTHERS">Others</option>
+                    </select>
+                </div> */}
+                <label htmlFor="yearOfStudy" className="input-labels">
+                    {" "}
+                    Year of Study{" "}
+                </label>
+                <br />
+
+                <div className='selectdropdown'>
+                    <select id="yearOfStudy" name="yearOfStudy" required=""
+                        onChange={handleChange} class="mySelectArrow">
+                        <option value="" disabled selected id='dont-display'>
+                            Select your option
+                        </option>
+                        <option value="First Year">First Year</option>
+                        <option value="Second Year">Second Year</option>
+                        <option value="Third Year">Third Year</option>
+                        <option value="Fourth Year">Fourth Year</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>
+                <label htmlFor="isDualBooted" className="input-labels">
+                    Do you have dual booted laptop ?{" "}
+                </label>
+                <br />
+                <div className='selectdropdown'>
+                    <select id="isDualBooted" name="isDualBooted" required="" onChange={handleChange} class="mySelectArrow">
+                        <option value="" disabled selected id='dont-display'>
+                            Select your option
+                        </option>
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                    </select>
+                </div>
+                <label htmlFor="transactionId" className="input-labels">
+                    {" "}
+                    Transaction ID{" "}
+                </label>
+                <input
+                    required=""
+                    placeholder="Payment Transaction ID"
+                    name="transactionId"
+                    id="transactionId"
+                    type="text"
+                    onChange={handleChange}
+                />
+                <label htmlFor="referalCode" className="input-labels">
+                    {" "}
+                    Referral Code{" "}
+                </label>
+                <input
+                    required=""
+                    placeholder="Referral Code"
+                    name="referalCode"
+                    id="referalCode"
+                    type="text"
+                    onChange={handleChange}
+                />
+                <div className='submitSection'>
+                    {!isLoading && (<div className='submitButton'>
+                        <input type="submit" defaultValue="REGISTER" className='btn-hover color-5' />
+                    </div>
+                    )}
+                    {isLoading && (
+                        <ProgressBar
+                            height="160"
+                            width="auto"
+                            ariaLabel="progress-bar-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="progress-bar-wrapper"
+                            borderColor='#00d12d'
+                            barColor='#ffffff'
+                        />
+                    )}
+
+                </div>
+            </form>
+            <div className='qr-div'>
+                {/* <img src={qr_img_src} className="qr" />
+                <a href={qrLink}>
+                <QRCode value={qrLink} className='qrWithName'></QRCode>
+            </a> */}
+                <div className='qr-section'>
+                    <div className='qr-text-section'>
+                        {/* <p className='qr-text-heading'>Scan <span>/ Tap </span>QR to pay</p> */}
+                        <p className='qr-text-heading'>Scan QR to pay</p>
+                        <p className='qr-text-info'>
+                            UPI ID: dattnareshgangji21@okaxis<br /><br />
+                            <span>Amount: ₹‎199.00</span></p>
+                    </div>
+                    {/* <div style={placeholderVisibility}>
+                        <p className='qr-text-placeholder'>Enter Name <br /> to view QR</p>
+                    </div> */}
+                    {/* <div style={qrVisibility}> */}
+                    <div>
+                        {/* <a href={qrLink}> */}
+                        {/* <a href="upi://pay?pa=dattnareshgangji21@okaxis&pn=Datta%20Gangji&am=1.00&cu=INR&aid=uGICAgID3ib3mVA"> */}
+                        <img src="./images/qr.png" className="qr2" alt='qr' />
+                        {/* <QRCode value={qrLink} className='qr'>
+                            </QRCode>*/}
+                        {/* </a>  */}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Register
